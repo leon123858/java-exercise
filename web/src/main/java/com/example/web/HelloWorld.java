417f0f145b2c8a6b6f09ffb3995a43773133b8d7
leon123858 <a0970785699@gmail.com>
2023-09-15 00:29:44 +0800

    add spring sample

---
+++ web/src/main/java/com/example/web/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.web;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
