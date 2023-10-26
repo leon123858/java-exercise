ca653fc26326ba6fb19a283de29482e3a2f51ae9
leon123858 <a0970785699@gmail.com>
2023-10-27 01:13:55 +0800

    init HW

---
+++ pattern-null/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String str= sc.nextLine();              //reads string
+        System.out.print("You have entered: "+str);
+    }
+}
