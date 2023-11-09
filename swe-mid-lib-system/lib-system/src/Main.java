4c8d1231fa0c6682a5633dd01bf1618c84e3108c
leon123858 <a0970785699@gmail.com>
2023-11-09 13:10:26 +0800

    edit output

--- swe-mid-lib-system/lib-system/src/Main.java
+++ swe-mid-lib-system/lib-system/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Wrong Input");
-//            return;
-            args = new String[1];
-            args[0] = "./test/sample.in";
+            System.out.println("Error");
+            return;
         }
         String inputFilePath = args[0];
         CommandRunner.run(inputFilePath);
