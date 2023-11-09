3e159580dcf6ec7aabb1bfb2817a4a5de7326edd
leon123858 <a0970785699@gmail.com>
2023-11-09 12:55:18 +0800

    input change

--- swe-mid-lib-system/lib-system/src/Main.java
+++ swe-mid-lib-system/lib-system/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        CommandRunner.run("./src/sample.in");
+        if (args.length != 1) {
+            System.out.println("Wrong Input");
+//            return;
+            args = new String[1];
+            args[0] = "./test/sample.in";
+        }
+        String inputFilePath = args[0];
+        CommandRunner.run(inputFilePath);
     }
 }
