f4178a1b86fb8af56cb9cbe53eb83c835ebf531d
leon123858 <a0970785699@gmail.com>
2023-12-02 01:36:18 +0800

    update

--- pattern-iterater/src/Main.java
+++ pattern-iterater/src/Main.java
@@ -8,10 +8,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
-//            System.out.println("Wrong Input");
-//            return;
-            args = new String[1];
-            args[0] = "./src/sampleInput";
+            return;
+//            args = new String[1];
+//            args[0] = "./src/sampleInput";
         }
         String inputFilePath = args[0];
         HashMap<String, CustomList> dataStructures = new HashMap<>();
