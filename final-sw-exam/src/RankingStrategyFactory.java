fb6eb873259817835f3d0abbdaccdc5ea1f33e1f
adamhung <touycvbn95147@gmail.com>
2023-12-21 12:39:40 +0800

    add factory

--- final-sw-exam/src/RankingStrategyFactory.java
+++ final-sw-exam/src/RankingStrategyFactory.java
@@ -2,6 +2,8 @@ public class RankingStrategyFactory {
     public static RankingStrategy create(String rankingStrategy) {
         if (rankingStrategy.equals("MeanRankingStrategy")) {
             return new MeanRankingStrategy();
+        } else if (rankingStrategy.equals("MedianRankingStrategy")) {
+                return new MedianRankingStrategy();
         } else {
             throw new IllegalArgumentException("Invalid ranking strategy");
         }
