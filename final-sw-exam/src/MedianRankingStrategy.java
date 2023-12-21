b7caef7c278d17345872bae4c69ee3156fcf5e32
adamhung <touycvbn95147@gmail.com>
2023-12-21 12:33:35 +0800

    add MedianRankingStrategy

--- final-sw-exam/src/MedianRankingStrategy.java
+++ final-sw-exam/src/MedianRankingStrategy.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MedianRankingStrategy implements RankingStrategy {
     @Override
     public double calculate(List<Rank> ranks) {
+        var scores = new ArrayList<>(ranks.stream().map(r -> r.getLevel().getScore()).toList());
 
-        var scores = ranks.stream().map(r -> r.getLevel().getScore()).toList();
+        scores.sort(Double::compareTo);
+        int length = scores.size();
 
-        //TODO
-        return 0;
+        if (length % 2 == 0) {
+            int middle1 = length / 2 - 1;
+            int middle2 = length / 2;
+            return (scores.get(middle1) + scores.get(middle2)) / 2.0;
+        } else {
+            int middle = length / 2;
+            return scores.get(middle);
+        }
     }
 }
